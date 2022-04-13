0f2137a4f723eb64bb519b286e149e39ba6ae2d4
Daniel Alcazar <danielalcazar54@gmail.com>
2022-04-12 22:09:18 -0300

    MOD estilo nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,40 +4,40 @@ const NavBar = () => {
     return (
             <header className="header">
                 <nav className="navbar navbar-expand-lg navbar-light nav__custom">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="../public/index.html">
-                        <div className="container fluid">
-                            <img className="d-inline col-2" src={logo} alt="wheelie-logo"/>
-                            <h1 className="d-inline col-2 px-3">Wheelie Bikes</h1>
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="../public/index.html">
+                            <div className="container fluid">
+                                <img className="d-inline col-2" src={logo} alt="wheelie-logo"/>
+                                <h1 className="d-inline col-2 px-3">Wheelie Bikes</h1>
+                            </div>
+                        </a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" aria-current="page" href="#">Bicicletas</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Componentes</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Accesorios</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Indumentaria</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">OFERTAS</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
+                            <button className="btn btn-outline-success" type="submit">Ir</button>
+                        </form>
                         </div>
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">Bicicletas</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Componentes</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Accesorios</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Indumentaria</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">OFERTAS</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
-                        <button className="btn btn-outline-success" type="submit">Ir</button>
-                    </form>
                     </div>
-                </div>
                 </nav>
             </header>
         )
